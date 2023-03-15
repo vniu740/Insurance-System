1731be5da0e1063d748fb8cc7c458665d664a449
VENXIA <vniu740>
2023-03-15 16:15:30 +1300

    Added code inside of createNewProfile method in InsuraceSystem.java to title case the username input

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -3,16 +3,25 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
+    
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+    System.out.println("Database has 0 profiles.");
   }
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
+
+    // Change the username to title case
+    userName = userName.toLowerCase();
+    userName = userName.substring(0, 1).toUpperCase() + userName.substring(1); 
+
+    
   }
 
   public void loadProfile(String userName) {
