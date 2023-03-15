26d8890e26da5da26ea00c11d8e240e112f9c431
VENXIA <vniu740>
2023-03-15 17:18:39 +1300

    Added code to CreateNewProfile method in InsuranceSystem.java to check if input username and input age are valid

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -4,10 +4,9 @@ import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
 
-
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-    
+
   }
 
   public void printDatabase() {
@@ -16,12 +15,22 @@ public class InsuranceSystem {
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
-
-    // Change the username to title case
+    // Change the username input to title case
     userName = userName.toLowerCase();
-    userName = userName.substring(0, 1).toUpperCase() + userName.substring(1); 
+    userName = userName.substring(0, 1).toUpperCase() + userName.substring(1);
 
-    
+    // Check if username input is lesss than 3 letters, if it is
+    if (userName.length() < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    }
+
+    // Check if age input is a positive integer
+    for (int i = 0; i < age.length(); i++) {
+      if (!Character.isDigit(age.charAt(i))) {
+        MessageCli.INVALID_AGE.printMessage(age, userName);
+        break;
+      }
+    }
   }
 
   public void loadProfile(String userName) {
