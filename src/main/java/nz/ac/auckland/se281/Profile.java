79fec0f6f869ecefa88451356d140f8d9dffa314
VENXIA <vniu740>
2023-03-15 15:02:34 +1300

    defined Profile class in newly added file Profile.java and defined a Profile constructor

---
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se281;
+
+public class Profile {
+    private String userName;
+    private String age;
+
+    public Profile(String userName, String age) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+}
