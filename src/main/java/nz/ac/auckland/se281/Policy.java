e951f8448b634b43fc332e68512eed4e5f151701
VENXIA <vniu740>
2023-04-08 22:19:59 +1200

    Updated discount method in abstract Policy.java class so method accounts for all cases instead of only 2 policies, i.e 0, 1, 2, 3 or more policies

--- src/main/java/nz/ac/auckland/se281/Policy.java
+++ src/main/java/nz/ac/auckland/se281/Policy.java
@@ -11,11 +11,24 @@ public abstract class Policy {
   // all policies calculate base premium differently
   public abstract int calculateBasePremium(Profile loadedProfile);
 
-  // all policies get a discount if client has 2 policies in database
-  public int calculateDiscountFor2Policies(Profile profileOfPolicyOwner) {
+  // all policies get a discount if client has 2 or more policies in database
+  public int calculateDiscount(Profile profileOfPolicyOwner) {
     double discountAmount;
-    discountAmount = calculateBasePremium(profileOfPolicyOwner) * 0.1;
-    discountAmount = calculateBasePremium(profileOfPolicyOwner) - discountAmount;
-    return (int) discountAmount;
+    // if the client has two policies, return premium with discount for two policies
+    if (Integer.parseInt(profileOfPolicyOwner.getSizeOfArrayOfPolicies()) == 2) {
+      discountAmount = calculateBasePremium(profileOfPolicyOwner) * 0.1;
+      discountAmount = calculateBasePremium(profileOfPolicyOwner) - discountAmount;
+      return (int) discountAmount;
+    
+    // if the client has 3 or more policies, return premium with discount for three or more policies
+    } else if (Integer.parseInt(profileOfPolicyOwner.getSizeOfArrayOfPolicies()) > 2) {
+      discountAmount = calculateBasePremium(profileOfPolicyOwner) * 0.2;
+      discountAmount = calculateBasePremium(profileOfPolicyOwner) - discountAmount;
+      return (int) discountAmount;
+    
+    // if client has 0 or 1 policy, dont apply a discount 
+    } else {
+      return Integer.parseInt(profileOfPolicyOwner.getSizeOfArrayOfPolicies());
+    }
   }
 }
