504d9bb1cd8ceecf83bf892ee191a8175f698b7c
VENXIA <vniu740>
2023-04-08 21:56:50 +1200

    Added calculateDiscountFor2Policies  method to abstract Policy.java class to calculate the discount if a client has 2 policies

--- src/main/java/nz/ac/auckland/se281/Policy.java
+++ src/main/java/nz/ac/auckland/se281/Policy.java
@@ -2,13 +2,20 @@ package nz.ac.auckland.se281;
 
 public abstract class Policy {
 
-    protected int sumInsured;
+  protected int sumInsured;
 
-    public Policy(int sumInsured) {
-        this.sumInsured = sumInsured;
-    }
+  public Policy(int sumInsured) {
+    this.sumInsured = sumInsured;
+  }
 
-    // all policies calculate base premium differently
-    public abstract int calculateBasePremium(Profile loadedProfile);
-    
+  // all policies calculate base premium differently
+  public abstract int calculateBasePremium(Profile loadedProfile);
+
+  // all policies get a discount if client has 2 policies in database
+  public int calculateDiscountFor2Policies(Profile profileOfPolicyOwner) {
+    double discountAmount;
+    discountAmount = calculateBasePremium(profileOfPolicyOwner) * 0.1;
+    discountAmount = calculateBasePremium(profileOfPolicyOwner) - discountAmount;
+    return (int) discountAmount;
+  }
 }
